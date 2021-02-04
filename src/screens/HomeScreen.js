6f3d069e495a5bed46318ae09f743a47695f0776
AtomicSpirit <park.daniel1@gmail.com>
2021-02-04 15:49:47 -0500

    feat Added User login capability

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import AuthForm from "../components/AuthForm";
+import {connect} from 'react-redux'
+import {auth} from '../store/user'
 
-export default function HomeScreen({ navigation }) {
+function HomeScreen({ navigation, login, user}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formType, setFormType] = useState("login") 
+  const onButtonPress = () => {
+    login(email, password, formType)
+    console.log("user", user)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Welcome To Vitamon!</Text>
+      <AuthForm
+        formType={formType}
+        email={email}
+        password={password}
+        onEmailChange={(newEmail) => setEmail(newEmail)}
+        onPasswordChange={(newPassword) => setPassword(newPassword)}
+        onPress = {() => onButtonPress()}
+      />
+
       <Button
         title="Go To Steps Screen"
         onPress={() => {
@@ -25,3 +45,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const mapState = (state) => {
+  return{
+    user: state.user
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    login: (email, password, formType) => {dispatch(auth(email, password, formType))}
+  };
+};
+
+export default connect(mapState, mapDispatch)(HomeScreen)
---
+++ src/store/user.js
@@ -0,0 +1,68 @@
+import vitamon from '../api/vitamon'
+
+/**
+ * ACTION TYPES
+ */
+const GET_USER = 'GET_USER'
+const REMOVE_USER = 'REMOVE_USER'
+
+/**
+ * INITIAL STATE
+ */
+const defaultUser = {}
+
+/**
+ * ACTION CREATORS
+ */
+const getUser = user => ({type: GET_USER, user})
+const removeUser = () => ({type: REMOVE_USER})
+
+/**
+ * THUNK CREATORS
+ */
+export const me = () => async dispatch => {
+  try {
+    const res = await vitamon.get('/auth/me')
+    dispatch(getUser(res.data || defaultUser))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const auth = (email, password, method) => async dispatch => {
+  let res
+  try {
+    res = await vitamon.post(`/auth/${method}`, {email, password})
+  } catch (authError) {
+    return dispatch(getUser({error: authError}))
+  }
+
+  try {
+    dispatch(getUser(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const logout = () => async dispatch => {
+  try {
+    await vitamon.post('/auth/logout')
+    dispatch(removeUser())
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultUser, action) {
+  switch (action.type) {
+    case GET_USER:
+      return action.user
+    case REMOVE_USER:
+      return defaultUser
+    default:
+      return state
+  }
+}
