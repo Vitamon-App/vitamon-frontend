bddb94fbafd5701442724ba318d7d1d4696116bb
AtomicSpirit <park.daniel1@gmail.com>
2021-02-06 10:47:41 -0500

    icon of welcome, steps, friend

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import WelcomeScreen from "./src/screens/WelcomeScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 import { Provider } from "react-redux";
 import store from "./src/store";
-import { SimpleLineIcons, FontAwesome5 } from "@expo/vector-icons";
+import { SimpleLineIcons, FontAwesome5, FontAwesome } from "@expo/vector-icons";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AllFriendsScreen from "./src/screens/AllFriendsScreen";
@@ -19,9 +19,39 @@ const Tab = createBottomTabNavigator();
 function NavTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Welcome" component={WelcomeScreen} />
-      <Tab.Screen name="Steps" component={StepsScreen} />
-      <Tab.Screen name="Friends" component={AllFriendsScreen} />
+      <Tab.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          //tabBarColor: "#2F004C",
+          tabBarIcon: ({ color }) => (
+            <SimpleLineIcons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Steps"
+        component={StepsScreen}
+        options={{
+          tabBarLabel: "Steps",
+          //tabBarColor: "#2F004C",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="shoe-prints" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Friends"
+        component={AllFriendsScreen}
+        options={{
+          tabBarLabel: "Friends",
+          //tabBarColor: "#2F004C",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="group" size={24} color="black" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -36,38 +66,9 @@ function App() {
           <Stack.Screen name="SignUp" component={SignUpScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-
-      {/* <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Steps" component={StepsScreen} />
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-
-          <Stack.Screen name="SignUp" component={SignUpScreen} />
-
-          <Stack.Screen name="Friends" component={AllFriendsScreen} />
-
-        </Stack.Navigator>
-      </NavigationContainer>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Home"
-            options={{
-              tabBarLabel: "Home",
-              //tabBarColor: "#2F004C",
-              tabBarIcon: ({ color }) => (
-                <SimpleLineIcons name="home" size={24} color={color} />
-              ),
-            }}
-            component={HomeScreen}
-          />
-        </Tab.Navigator>
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-
-      {/* <MyTabBar /> */}
-      {/* </NavigationContainer>  */}
     </Provider>
   );
 }
 
 export default App;
+cody
