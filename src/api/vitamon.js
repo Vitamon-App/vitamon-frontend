f81efc6635d1f50e8fbcbf9ed0fc450f8311596d
Priscila <priscilapintado66@gmail.com>
2021-02-08 14:01:15 -0500

    fix: letters are not allowed in the inputs of addGoalForm

--- src/api/vitamon.js
+++ src/api/vitamon.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const ip = "192.168.1.4";
+const ip = "192.168.4.222";
 //daniel "192.168.1.165"
 //veronica 192.168.1.4
 //melissa 192.168.1.243
 //priscila 192.168.4.222
-
 
 export default axios.create({
   baseURL: `http://${ip}:8080`,
