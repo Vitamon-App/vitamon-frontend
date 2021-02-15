7133d91139a6bec63d34b5c4e3cb2dc86e870b40
AtomicSpirit <park.daniel1@gmail.com>
2021-02-15 16:16:17 -0500

    removed leaderboard

--- src/screens/AllFriendsScreen.js
+++ src/screens/AllFriendsScreen.js
@@ -1,27 +1,13 @@
 import React from "react";
-import {
-  StyleSheet,
-  // Text,
-  View,
-  FlatList,
-  Image,
-  Dimensions,
-  TouchableOpacity,
-  Platform,
-  ScrollView,
-} from "react-native";
-
+import { StyleSheet, View, Dimensions, ScrollView } from "react-native";
 import { connect } from "react-redux";
 import { fetchFriends } from "../store/friends";
-import GoalsOfFriends from "../components/GoalsOfFriends"
+
 // Galio components
 import { Text, Card, Block, NavBar, Icon, Button } from "galio-framework";
 import theme from "../theme";
 
 const width = Dimensions.get("window").width;
-
-
-
 
 class AllFriendsScreen extends React.Component {
   async componentDidMount() {
@@ -29,38 +15,8 @@ class AllFriendsScreen extends React.Component {
   }
 
   render() {
-    const user = this.props.user
     const friends = this.props.friends || [];
 
-    console.log(friends)
-    friends.push(this.props.user)
-    const {navigation} = this.props
-    let leaderBoard = {}
-    friends.forEach((friend) => {
-       leaderBoard[`${friend.name}`] = 0
-        friend.goals.forEach(goal =>{
-          if(goal.status === "complete"){
-          leaderBoard[`${friend.name}`]++
-          }} )
-         
-       })
-       //console.log("leaderBoard", leaderBoard)
-       friends.pop()
-    let ranking = Object.entries(leaderBoard)
-    let highest = []
-    //console.log("ranking", ranking.sort())
-    ranking.forEach(rank => {
-      if(rank[1] > 0){
-        highest = rank
-      }
-    if (highest[1] < rank[1]){
-        highest = rank
-      }
-    })
-    if(highest[0] === user.name){
-      highest[0] = "You are"
-    }
-    
     return (
       <Block safe flex style={{ backgroundColor: theme.COLORS.WHITE }}>
         <ScrollView contentContainerStyle={styles.cards}>
@@ -69,11 +25,7 @@ class AllFriendsScreen extends React.Component {
               {" "}
               Here are all your friends!{" "}
             </Text>
-            <Text style={styles.goalLeader}>
-              {highest.length === 0
-                ? "None of your friends have completed goals to be" :
-                 `${highest[0]} is`} the goal leader
-            </Text>
+
             {friends.length ? (
               friends.map((friend, id) => (
                 <Card
@@ -82,18 +34,19 @@ class AllFriendsScreen extends React.Component {
                   borderless
                   shadowColor={theme.COLORS.BLACK}
                   titleColor={theme.COLORS.WHITE}
-
                   captionColor={theme.COLORS.WHITE}
                   style={styles.card}
-                  //title={friend.name}
-                  //caption={friend.email}
                   location={
                     <View style={styles.buttonContainer}>
                       <Button
                         round
                         size="small"
                         color="#5539AA"
-                        onPress={() => this.props.navigation.navigate("FriendsGoals", {friend: friend})}
+                        onPress={() =>
+                          this.props.navigation.navigate("FriendsGoals", {
+                            friend: friend,
+                          })
+                        }
                       >
                         GOALS
                       </Button>
@@ -102,19 +55,12 @@ class AllFriendsScreen extends React.Component {
                   //avatar={`https://images.unsplash.com/photo-1571172964276-91faaa704e1f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80`}
                   image={`${friend.imageUrl}`}
                   imageStyle={styles.cardImageRadius}
-
                   imageStyle={styles.rounded}
-                  imageBlockStyle={[
-                    // { padding: theme.SIZES.BASE / 2 },
-                    styles.noRadius,
-                  ]}
+                  imageBlockStyle={[styles.noRadius]}
                   footerStyle={styles.full}
-
                 >
-                 
                   <Text style={styles.textStyle}>{friend.name}</Text>
                 </Card>
-
               ))
             ) : (
               <Text style={{ marginVertical: theme.SIZES.FONT / 4 }} h1>
@@ -132,7 +78,6 @@ class AllFriendsScreen extends React.Component {
                 Add a new friend!
               </Button>
             </View>
-
           </Block>
         </ScrollView>
       </Block>
@@ -177,7 +122,7 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: theme.SIZES.BASE * 0.5,
   },
   primary: {
-    backgroundColor: theme.COLORS.primary
+    backgroundColor: theme.COLORS.primary,
   },
 
   buttonContainer: {
@@ -185,20 +130,12 @@ const styles = StyleSheet.create({
     width: 5,
     marginTop: 19,
     marginBottom: 20,
-    //marginHorizontal: 10,
     marginRight: 70,
     backgroundColor: "#97A5E9",
     alignItems: "center",
     justifyContent: "center",
   },
   buttonContainer2: {
-    /*    height: 40,
-    width: 5,
-    marginTop: 19,
-    marginBottom: 10, */
-    //marginHorizontal: 10,
-    //marginLeft: 90,
-    //backgroundColor: "#97A5E9",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -207,16 +144,8 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "bold",
     textAlign: "center",
-    //marginTop: 10,
-    //width: 200,
     backgroundColor: "#5539AA",
   },
-
-  goalLeader : {
-    fontSize: 18,
-    textAlign: "center"
-  }
-
 });
 
 const mapState = (state) => {
